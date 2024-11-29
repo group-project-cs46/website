ad9d7234161de068cb63ced0f3e679d4cfefaf53
naveen.sack <pmdwijegunasinghe@gmail.com>
2024-11-29 06:42:12 +0530

    edit

--- views/partials/auth/auth.php
+++ views/partials/auth/auth.php
@@ -1,5 +1,2 @@
 <?php require base_path('views/partials/head.php') ?>
 <?php require base_path('views/partials/nav.php') ?>
-<!-- <div style="display: grid-template-columns: 50px 1fr; gap:0.5rem;">
-    <div></div>
-</div> -->
