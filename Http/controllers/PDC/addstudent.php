09a920ef9c1ee2f674a9575cbabf72d695eae445
nova9 <thathsaramadhhusha@gmail.com>
2025-01-22 12:26:40 +0530

    pdc added students can now login

--- Http/controllers/PDC/addstudent.php
+++ Http/controllers/PDC/addstudent.php
@@ -1,25 +1,35 @@
-<?php 
+<?php
 
 use Models\AddStudent;
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST["name"] ?? null;
-    $indexno = $_POST["indexno"] ?? null;
-    $regNo = $_POST["regNo"] ?? null;
-    $email = $_POST["email"] ?? null;
-    $course = $_POST["course"] ?? null;
+use Core\App;
+use Models\User;
 
-    if ($name && $indexno && $regNo && $email && $course) {
-        $password = password_hash($indexno, PASSWORD_DEFAULT);
-        //AddStudent::create_user($name, $email, $password);
-        AddStudent::create_student($regNo, $course, $email, $name, $indexno);
-       
 
-        header('Location: /PDC/managestudents');
-        exit; // Ensure the script stops after the redirect
-    } else {
-        // Handle missing fields (optional)
-        echo "All fields are required.";
-        exit;
-    }
+$name = $_POST["name"] ?? null;
+$indexno = $_POST["indexno"] ?? null;
+$regNo = $_POST["regNo"] ?? null;
+$email = $_POST["email"] ?? null;
+$course = $_POST["course"] ?? null;
+
+if ($name && $indexno && $regNo && $email && $course) {
+    User::create(
+        [
+            'name' => $name,
+            'email' => $email,
+            'password' => $indexno,
+            'role' => 2,
+            'approved' => 1,
+        ]
+    );
+    //AddStudent::create_user($name, $email, $password);
+    AddStudent::create_student($regNo, $course, $email, $name, $indexno);
+
+
+    header('Location: /PDC/managestudents');
+    exit; // Ensure the script stops after the redirect
+} else {
+    // Handle missing fields (optional)
+    echo "All fields are required.";
+    exit;
 }
