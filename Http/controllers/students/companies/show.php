b9f7a79ecd78a8f89255aea227739ba3f4ffaf50
nova9 <thathsaramadhhusha@gmail.com>
2025-02-03 13:01:13 +0530

    company profile view for students

--- Http/controllers/students/companies/show.php
+++ Http/controllers/students/companies/show.php
@@ -1,6 +1,13 @@
 <?php
 
+use Models\User;
+
+$user = User::findByIdWithRoleData($_GET['id']);
+
+//dd($user);
 
 view('students/companies/show.view.php', [
+    'user' => $user,
     'heading' => 'jkj',
+    'photo' => getUserProfilePhotoUrl($user)
 ]);
