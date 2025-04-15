6bed59441013d4ebd5ae7217103f733e41c90f2d
nive19s <sothilingamnivethan19@gmail.com>
2025-04-15 14:55:04 +0530

    commit pdc

--- Http/controllers/admin/lecturerEdit.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-view('admin/lecturerEdit.view.php', []);
--- Models/TechTalk.php
+++ Models/TechTalk.php
@@ -11,12 +11,12 @@ class TechTalk
     {
         $db = App::resolve(Database::class);
 
-        $techtalks = $db->query('SELECT * FROM techtalk_slots ts
-LEFT JOIN techtalks t ON ts.id = t.techtalk_slot_id
+        $techtalks = $db->query('SELECT * FROM techtalks t
+LEFT JOIN techtalk_slots ts ON ts.id = t.techtalk_slot_id
          LEFT JOIN companies c ON t.company_id = c.id
          LEFT JOIN users u ON c.id = u.id
-WHERE EXTRACT(MONTH FROM time) = EXTRACT(MONTH FROM CURRENT_DATE) 
-AND EXTRACT(YEAR FROM time) = EXTRACT(YEAR FROM CURRENT_DATE);', [])->get();
+WHERE EXTRACT(MONTH FROM datetime) = EXTRACT(MONTH FROM CURRENT_DATE) 
+AND EXTRACT(YEAR FROM datetime) = EXTRACT(YEAR FROM CURRENT_DATE);', [])->get();
 
         return $techtalks;
     }
