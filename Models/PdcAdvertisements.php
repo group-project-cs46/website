a83d80ca565e7ff8e5a37b28ed012c457dd75efa
nive19s <sothilingamnivethan19@gmail.com>
2025-04-22 03:07:12 +0530

    advertisements completed

--- Models/PdcAdvertisements.php
+++ Models/PdcAdvertisements.php
@@ -12,10 +12,13 @@ class PdcAdvertisements
         $db = App::resolve(Database::class);
         return $db->query('
             SELECT 
-                ad.*, 
-                users.name AS company_name,users.email AS company_email
-            FROM advertisements ad
-            JOIN users ON ad.company_id = users.id
+            ad.*, 
+            internship_roles.name AS job_role,
+            users.name AS company_name,
+            users.email AS company_email
+        FROM advertisements ad
+        JOIN users ON ad.company_id = users.id
+        JOIN internship_roles ON ad.internship_role_id = internship_roles.id
             WHERE ad.approved IS NULL
         ', [])->get();
     }
@@ -25,26 +28,32 @@ class PdcAdvertisements
         $db = App::resolve(Database::class);
         return $db->query('
             SELECT 
-                ad.*, 
-                users.name AS company_name,users.email AS company_email
-            FROM advertisements ad
-            JOIN users ON ad.company_id = users.id
+            ad.*, 
+            internship_roles.name AS job_role,
+            users.name AS company_name,
+            users.email AS company_email
+        FROM advertisements ad
+        JOIN users ON ad.company_id = users.id
+        JOIN internship_roles ON ad.internship_role_id = internship_roles.id
             WHERE ad.approved = TRUE
         ', [])->get();
     }
 
     public static function fetchById($id)
-    {
-        $db = App::resolve(Database::class);
-        return $db->query('
-            SELECT 
-                advertisements.*, 
-                users.name AS company_name
-            FROM advertisements 
-            JOIN users ON advertisements.company_id = users.id
-            WHERE advertisements.id = ?
-        ', [$id])->find();
-    }
+{
+    $db = App::resolve(Database::class);
+    return $db->query('
+        SELECT 
+            ad.*, 
+            internship_roles.name AS job_role,
+            users.name AS company_name,
+            users.email AS company_email
+        FROM advertisements ad
+        JOIN users ON ad.company_id = users.id
+        JOIN internship_roles ON ad.internship_role_id = internship_roles.id
+        WHERE ad.id = ?
+    ', [$id])->find();
+}
 
     public static function reject($id)
     {
@@ -57,4 +66,22 @@ class PdcAdvertisements
         $db = App::resolve(Database::class);
         $db->query('UPDATE advertisements SET approved = TRUE WHERE id = ?', [$id]);
     }
+
+    public static function fetchappliedstudents($id)
+{
+    $db = App::resolve(Database::class);
+    return $db->query('
+        SELECT
+            a.*, ad.*,
+            users.name,
+            s.registration_number,
+            users.email
+        FROM applications a
+        JOIN students s ON a.student_id = s.id
+        JOIN users ON s.id = users.id
+        JOIN advertisements ad ON a.ad_id = ad.id
+        WHERE a.ad_id = ?
+    ', [$id])->get();
+}
+
 }
