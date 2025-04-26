091aba936c15343875687d84044af3073f44f869
Karunya1912 <raveendrankarunya01@gmail.com>
2025-04-26 16:50:06 +0530

    new changes in validation ads

--- Http/Forms/AdvertisementUpdate.php
+++ Http/Forms/AdvertisementUpdate.php
@@ -22,16 +22,32 @@ class AdvertisementUpdate extends BaseForm
             $this->error('qualifications_skills', 'Qualifications skills are required')->throw();
         }
 
+        // Validate max_cvs: must be a positive number
         if (!Validator::string($this->attributes['max_cvs'], 1, 255)) {
-            $this->error('max_cvs', 'Max CVs is required')->throw();
+            $this->error('max_cvs', 'Maximum number of resumes is required')->throw();
+        }
+        $maxCvs = (int)$this->attributes['max_cvs'];
+        if ($maxCvs <= 0) {
+            $this->error('max_cvs', 'Maximum number of resumes must be a positive number')->throw();
         }
 
+        // Validate deadline: must not be a past date
+        $currentDate = new \DateTime(); // Current date
+        $deadlineDate = \DateTime::createFromFormat('Y-m-d', $this->attributes['deadline']);
         if (!Validator::string($this->attributes['deadline'], 1, 255)) {
             $this->error('deadline', 'Deadline is required')->throw();
         }
+        if ($deadlineDate === false || $deadlineDate < $currentDate) {
+            $this->error('deadline', 'Deadline cannot be a past date')->throw();
+        }
 
+        // Validate vacancy_count: must be a positive number
         if (!Validator::string($this->attributes['vacancy_count'], 1, 255)) {
-            $this->error('vacancy_count', 'Vacancy count is required')->throw();
+            $this->error('vacancy_count', 'Number of vacancies is required')->throw();
+        }
+        $vacancyCount = (int)$this->attributes['vacancy_count'];
+        if ($vacancyCount <= 0) {
+            $this->error('vacancy_count', 'Number of vacancies must be a positive number')->throw();
         }
     }
 }
